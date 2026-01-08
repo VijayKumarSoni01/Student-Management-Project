1f4a48ee49239e1bbfe9614ba6d186e42090b544
Vijay Kumar Soni <kumarvijaysoni4@gmail.com>
2026-01-08 11:44:56 +0530

    Integrate Eureka client and Feign in AttendanceManagement service

--- AttendanceManagement/AttendanceManagement/src/main/java/com/Attendance/AttendanceManagement/AttendanceManagementApplication.java
+++ AttendanceManagement/AttendanceManagement/src/main/java/com/Attendance/AttendanceManagement/AttendanceManagementApplication.java
@@ -2,8 +2,10 @@ package com.Attendance.AttendanceManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AttendanceManagementApplication {
 
 	public static void main(String[] args) {
--- AttendanceManagement/AttendanceManagement/src/main/java/com/Attendance/AttendanceManagement/client/StudentClient.java
+++ AttendanceManagement/AttendanceManagement/src/main/java/com/Attendance/AttendanceManagement/client/StudentClient.java
@@ -1,24 +1,14 @@
 package com.Attendance.AttendanceManagement.client;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.Attendance.AttendanceManagement.dto.StudentResponseDto;
 
-@Service
-public class StudentClient {
+@FeignClient(name = "student-service")
+public interface StudentClient {
 
-    @Autowired
-    private RestTemplate restTemplate;
-
-    private static final String STUDENT_URL =
-            "http://localhost:8080/api/students/";
-
-    public StudentResponseDto getStudent(Long rollNo) {
-        return restTemplate.getForObject(
-                STUDENT_URL + rollNo,
-                StudentResponseDto.class
-        );
-    }
+    @GetMapping("/api/students/{rollNo}")
+    StudentResponseDto getStudent(@PathVariable("rollNo") Long rollNo);
 }
